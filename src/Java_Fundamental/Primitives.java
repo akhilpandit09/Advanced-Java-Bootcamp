23c2f0851460a8b469323958a6d2126eecfbcb56
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2023-10-30 09:50:11 +0545

    Added primitives.java

---
+++ src/Java_Fundamental/Primitives.java
@@ -0,0 +1,12 @@
+package Java_Fundamental;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int roll_no = 23;
+        char letter = 'r';
+        float marks = 45.64f;
+        double largeDecimalNumber = 343532342.45234;
+        long largerInteger = 343253325235235235L;
+        boolean check = false;
+    }
+}
